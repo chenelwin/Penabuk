94355938adb89ecf14e4d4c63edc4745a8a974fe
chenelwin <elwinchen@rocketmail.com>
2019-01-29 23:28:27 +0700

    UI login, chg pwd, fgt pwd, topup

--- app/src/main/java/com/example/asus/penabuk/LoginActivity.java
+++ app/src/main/java/com/example/asus/penabuk/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.asus.penabuk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    TextView textForgotPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+
+        textForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView(){
+        textForgotPassword = (TextView)findViewById(R.id.textForgotPassword);
     }
 }
