a7bd7589befc387cb3b316022c615761955222e9
chenelwin <elwinchen@rocketmail.com>
2019-01-30 23:02:40 +0700

    UI regis

--- app/src/main/java/com/example/asus/penabuk/LoginActivity.java
+++ app/src/main/java/com/example/asus/penabuk/LoginActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     TextView textForgotPassword;
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +25,19 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void initView(){
         textForgotPassword = (TextView)findViewById(R.id.textForgotPassword);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
     }
 }
